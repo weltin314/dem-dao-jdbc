d33dd9a86ddf15ff5d26a6f8ead8b167f4f7e08e
Wellinton Sousa Pires <weltin.max@gmail.com>
2026-03-13 14:23:47 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,9 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
 
     public static void main(String[] args) {
@@ -14,6 +17,14 @@ public class Program {
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
+
+        System.out.println("==== TEST ====");
+
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
 
     }
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -12,5 +12,6 @@ public interface SellerDao {
     void deleteByID(Integer id);
     Seller findById(Integer id);
     List<Seller> findAll();
+    List<Seller> findByDepartment(Department department);
 
 }
