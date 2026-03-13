f2dabe53b9a75307e393e571091ba1ed1bd4d020
Wellinton Sousa Pires <weltin.max@gmail.com>
2026-03-13 12:25:29 -0300

    findByID implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Department obj = new Department(12, "Books");
-        System.out.println(obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
 
     }
 }
